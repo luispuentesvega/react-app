27705aa139bc3e416376dbbb806b588e3f15194c
Luis Puentes <luispuentesvega@gmail.com>
2020-02-08 14:16:20 +0700

    Test automatic deploy

--- src/modules/actions/components/index.jsx
+++ src/modules/actions/components/index.jsx
@@ -1,32 +1,33 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import style from './style.scss';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import style from "./style.scss";
 
 const classNames = {
   appear: style.appear,
-  appearActive: style['appear-active'],
+  appearActive: style["appear-active"],
   enter: style.enter,
-  enterActive: style['enter-active'],
+  enterActive: style["enter-active"],
   exit: style.exit,
-  exitActive: style['exit-active'],
+  exitActive: style["exit-active"]
 };
 
 const transitionProps = {
   timeout: 1000,
-  classNames,
+  classNames
 };
 
 const Component = ({ status, initActions, clearActions }) => (
   <Fragment>
+    <h1>New Sample to test</h1>
     <h1>Status: {status}</h1>
     <TransitionGroup>
-      {status !== 'idle' && (
+      {status !== "idle" && (
         <CSSTransition {...transitionProps}>
           <button onClick={initActions}>Init</button>
         </CSSTransition>
       )}
-      {status === 'idle' && (
+      {status === "idle" && (
         <CSSTransition {...transitionProps}>
           <button onClick={clearActions}>Clear</button>
         </CSSTransition>
@@ -38,7 +39,7 @@ const Component = ({ status, initActions, clearActions }) => (
 Component.propTypes = {
   status: PropTypes.string.isRequired,
   initActions: PropTypes.func.isRequired,
-  clearActions: PropTypes.func.isRequired,
+  clearActions: PropTypes.func.isRequired
 };
 
 export default Component;
